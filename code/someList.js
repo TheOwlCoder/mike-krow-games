eec151cb82fb5c11e76ddeb474ed56447675d6e8
TheOwlCoder <100481826+TheOwlCoder@users.noreply.github.com>
2022-10-29 11:24:19 -0500

    f

--- code/someList.js
+++ code/someList.js
@@ -19,6 +19,17 @@
                 entry.appendChild(headline);
                 container.appendChild(entry);
             }
+            if (i=2) {
+                var entry = document.createElement("div");
+                entry.className = 'navItem';
+                entry.tabIndex = i * 10;
+                entry.setAttribute('data-function', 'changeColor');
+    
+                var headline = document.createElement("h3");
+                headline.innerText = 'another one!';
+                entry.appendChild(headline);
+                container.appendChild(entry);
+            }
         }
     }
 
